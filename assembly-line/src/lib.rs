a9f02d3bc63384d0f5ad8583afc365609d74be84
Gary Yendell <gary.yendell@diamond.ac.uk>
2022-10-18 23:25:10 +0100

    Improve exercism 5
    - Extract boilerplate multiplication by assigning value from match block

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -5,12 +5,14 @@
 static MAX_CARS_PER_HOUR: f64 = 221.0;
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    match speed {
-        0..=4 => speed as f64 * MAX_CARS_PER_HOUR,
-        5..=8 => speed as f64 * MAX_CARS_PER_HOUR * 0.9,
-        9..=10 => speed as f64 * MAX_CARS_PER_HOUR * 0.77,
+    let efficiency: f64 = match speed {
+        0..=4 => 1.0,
+        5..=8 => 0.9,
+        9..=10 => 0.77,
         _ => panic!("Illegal speed"),
-    }
+    };
+
+    speed as f64 * MAX_CARS_PER_HOUR * efficiency
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
