7ed5a31a25a7b9703e7122bb7b573334bc3bed40
Gary Yendell <gary.yendell@diamond.ac.uk>
2022-11-06 22:48:36 +0000

    Passing

--- tournament/src/lib.rs
+++ tournament/src/lib.rs
@@ -1,26 +1,38 @@
+use std::cmp;
 use std::collections::HashMap;
 
 fn format_row(t: &str, m: &str, w: &str, d: &str, l: &str, p: &str) -> String {
     format!(
-        "{:<30} |{:^4}| {:^3}| {:^3}| {:^3}| {:>2}",
+        "{:<30} | {:^3}| {:^3}| {:^3}| {:^3}| {:>2}",
         t, m, w, d, l, p
     )
 }
 
-#[derive(Clone, Copy)]
-struct Results {
+#[derive(Clone, Eq)]
+struct TeamResult {
+    name: String,
     wins: i32,
     draws: i32,
     losses: i32,
 }
 
-impl Results {
-    fn new() -> Results {
-        Results {
+impl TeamResult {
+    fn new(name: &str) -> TeamResult {
+        TeamResult {
+            name: name.to_string(),
             wins: 0,
             draws: 0,
             losses: 0,
         }
+    }
+    fn add_win(&mut self) {
+        self.wins += 1;
+    }
+    fn add_draw(&mut self) {
+        self.draws += 1;
+    }
+    fn add_loss(&mut self) {
+        self.losses += 1;
     }
     fn played(&self) -> i32 {
         self.wins + self.draws + self.losses
@@ -28,41 +40,93 @@ impl Results {
     fn points(&self) -> i32 {
         self.wins * 3 + self.draws
     }
+    fn to_string(&self) -> String {
+        format_row(
+            &self.name,
+            &self.played().to_string(),
+            &self.wins.to_string(),
+            &self.draws.to_string(),
+            &self.losses.to_string(),
+            &self.points().to_string(),
+        )
+    }
+}
+
+impl PartialOrd for TeamResult {
+    fn partial_cmp(&self, other: &TeamResult) -> Option<cmp::Ordering> {
+        Some(other.cmp(self))
+    }
+}
+
+impl Ord for TeamResult {
+    fn cmp(&self, other: &TeamResult) -> cmp::Ordering {
+        if self.eq(other) {
+            other.name.cmp(&self.name)
+        } else {
+            self.points().cmp(&other.points())
+        }
+    }
+}
+
+impl PartialEq for TeamResult {
+    fn eq(&self, other: &TeamResult) -> bool {
+        self.points() == other.points()
+    }
+}
+
+struct LeagueResult {
+    teams: HashMap<String, TeamResult>,
+}
+
+impl LeagueResult {
+    fn new() -> LeagueResult {
+        LeagueResult {
+            teams: HashMap::new(),
+        }
+    }
+    fn add_team(&mut self, team: &str) {
+        if !self.teams.contains_key(team) {
+            self.teams.insert(team.to_string(), TeamResult::new(team));
+        }
+    }
+
+    fn record_win<'a>(&mut self, winner_name: &'a str, loser_name: &'a str) {
+        self.add_team(winner_name);
+        self.add_team(loser_name);
+
+        self.teams.get_mut(winner_name).unwrap().add_win();
+        self.teams.get_mut(loser_name).unwrap().add_loss();
+    }
+
+    fn record_draw<'a>(&mut self, team1_name: &'a str, team2_name: &'a str) {
+        self.add_team(team1_name);
+        self.add_team(team2_name);
+
+        self.teams.get_mut(team1_name).unwrap().add_draw();
+        self.teams.get_mut(team2_name).unwrap().add_draw();
+    }
+    fn sorted_teams(&self) -> Vec<&TeamResult> {
+        let mut teams: Vec<&TeamResult> = self.teams.values().to_owned().collect();
+        teams.sort();
+        teams
+    }
 }
 
 pub fn tally(match_results: &str) -> String {
-    let mut team_results: HashMap<&str, Results> = HashMap::new();
+    let mut league_result = LeagueResult::new();
     for result in match_results.split('\n') {
         match result.split(';').collect::<Vec<&str>>()[..] {
-            [winner, loser, "win"] => {
-                if !team_results.contains_key(winner) {
-                    team_results.insert(winner, Results::new());
-                }
-                if !team_results.contains_key(loser) {
-                    team_results.insert(loser, Results::new());
-                }
-
-                let mut winner_results = team_results.get_mut(winner).unwrap();
-                winner_results.wins += 1;
-
-                let mut loser_results = team_results.get_mut(loser).unwrap();
-                loser_results.losses += 1;
-            }
+            [winner, loser, "win"] => league_result.record_win(winner, loser),
+            [loser, winner, "loss"] => league_result.record_win(winner, loser),
+            [team1, team2, "draw"] => league_result.record_draw(team1, team2),
             _ => println!("Not implemented"),
         }
     }
 
     let mut table = format_row("Team", "MP", "W", "D", "L", "P");
-    for (team, r) in team_results {
+    for team_result in league_result.sorted_teams() {
         table.push('\n');
-        table.push_str(&format_row(
-            team,
-            &r.played().to_string(),
-            &r.wins.to_string(),
-            &r.draws.to_string(),
-            &r.losses.to_string(),
-            &r.points().to_string(),
-        ));
+        table.push_str(team_result.to_string().as_str());
     }
     table
 }
